3b9df4042d2f29dcd5a305da5d05da991ce6a8ec
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-13 07:55:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word(word)
   reverse_each_word = word.split("")
   reverse_each_word = []
   reverse_each_word.each do |word|
-    reverse_each_word.push word.array
+    reverse_each_word << word.array
   end
 end
